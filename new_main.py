0f381fe5e4861959f369035e7b8b2c58c5ed9782
Michele Palamidessi <palamidessimichele@gmail.com>
2018-07-04 08:02:28 +0200

    - elevation noise created from class instance

--- new_main.py
+++ new_main.py
@@ -6,11 +6,19 @@ from src.generators.elevation_generator import ElevationGenerator
 
 argv = sys.argv
 size = argv[1] if len(argv) > 1 else 100
+shape_octaves = argv[2] if len(argv) > 2 else 30
+height_octaves = argv[3] if len(argv) > 3 else 5
+height_seed = argv[4] if len(argv) > 4 else 0
+
+
 size = int(size)
+shape_octaves = int(shape_octaves)
+height_octaves = int(height_octaves)
+height_seed = int(height_seed)
 map = Map(size, size)
 
-shapeGenerator = ShapeGenerator(16.00, 30)
-elevationGenerator = ElevationGenerator(16.00, 5)
+shapeGenerator = ShapeGenerator(16.00, shape_octaves)
+elevationGenerator = ElevationGenerator(16.00, height_octaves, height_seed)
 map = shapeGenerator.generate(map)
 map = elevationGenerator.generate(map)
 map.elaborate()
--- src/generators/elevation_generator.py
+++ src/generators/elevation_generator.py
@@ -6,32 +6,36 @@ from src.generators.abstract_generator import AbstractGenerator
 
 class ElevationGenerator(AbstractGenerator):
 
-    def __init__(self, freq: float, octaves: int):
+    def __init__(self, freq: float, octaves: int, seed: int = 0):
         self.frequency = freq * octaves
         self.octaves = octaves
-        self.simplex_noise = SimplexNoise()
-        self.simplex_noise.randomize(10)
+        self.seed = seed
+        self.noise = SimplexNoise()
+        self.noise.randomize(self.seed)
 
     def generate(self, map: Map) -> Map:
         elevation_map = [[0 for i in range(map.height)] for j in range(map.width)]
 
         for y in range(map.height):
             for x in range(map.width):
-                elevation_map[y][x] = self.__get_elevation(y, x)
+                ny = (x/map.height -0.5)
+                nx = (y/map.width -0.5)
+                elevation_map[y][x] = self.__get_elevation(ny, nx)
 
         map.set_elevation_matrix(elevation_map)
 
         return map
 
     def __get_elevation(self, x : int, y : int):
-        noise = self.__get_noise(x, y)* 127 + 128
+        # noise = self.__get_noise(x, y)* 127 + 128
+        noise = self.__get_noise(x, y)
+
         return int(noise)
 
     def __get_noise(self, x: int, y: int):
-        noise = 0
-        # for octaves in range(self.octaves):
-        #     current = pow(2, octaves)
-        #     noise += self.simplex_noise.noise2(x *  current, y * current) / current
-        #
-        # return noise / self.octaves
-        return snoise2(x / self.frequency, y / self.frequency, self.octaves)
+        value = self.noise.noise2(x, y)
+        for i in range(self.octaves):
+            multiplier = pow(2, self.octaves)
+            value += 1 / multiplier * self.noise.noise2(x * multiplier, y * multiplier)
+        return int(value / 2 * 127 + 128)
+
